826f6a54e77ec8e5bc530213597f2793b223ac28
Ankit Maurya <146062096+Ankit-mauryaji@users.noreply.github.com>
2023-10-24 09:39:28 +0000

    Learn Variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,16 @@
+//tere are three type variable 
+//const:-this type of keyword are used to store a value once time, you can't change value after delcear
+//var:- this type of keyword are used to store a value you can change value anytime, now this is not use because scope problem 
+//let:-this type of keyword are used to store a value you can change value anytime
+
+const accountId=12345
+var accountName="Ankit";
+let accountEmail="ankit@gmail.com"
+accountCity="Azamgarh";
+let accountState="uttarpradesh";
+let accountCountry;
+let accountNumber="null";
+/*console.log(accountId);
+console.log(typeof(accountId));
+*/
+console.table([accountId,accountName,accountEmail,accountCity,accountState,accountCountry,accountNumber]);
